26db58936ad1e90bae9dbb72c6db8948e3d4377f
Jerry <jerry.wang@xuenn.com>
2021-01-27 17:04:59 +0800

    add html attr bind using v-bind:attr

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 new Vue({
     el: "#vue-app",
     data: {
-        name: "jerry"
+        name: "jerry",
+        movieUrl: "https://www.youtube.com/"
     },
     methods: {
         greet: function(st){
-            return "Good " + st;
+            return "Good " + st + ':'+ this.name;
         }
     }
 });
