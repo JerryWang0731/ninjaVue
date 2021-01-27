dfb7e484c4374cd12bf34b37961f8c219f2ad111
Jerry <jerry.wang@xuenn.com>
2021-01-27 17:22:07 +0800

    v-on bind event

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ new Vue({
     el: "#vue-app",
     data: {
         name: "jerry",
-        movieUrl: "https://www.youtube.com/"
+        movieUrl: "https://www.youtube.com/",
+        movieUrlTag: '<a href="https://www.youtube.com/">good stuff</a>',
+        age: 87,
     },
     methods: {
         greet: function(st){
             return "Good " + st + ':'+ this.name;
+        },
+        subtract: function()
+        {
+            this.age--;
         }
     }
 });
