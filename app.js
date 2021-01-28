068a4f5328ce373b10c8cd69f9167d85b1f1b683
Jerry <jerry.wang@xuenn.com>
2021-01-28 18:01:50 +0800

    use computed prop

--- app.js
+++ app.js
@@ -6,10 +6,9 @@ new Vue({
         y: 0,
     },
     methods: {
-        triggerOther: function(){
-            alert('trigger other');
-            return 'other';
-        },
+       
+    },
+    computed: {
         addX: function(){
             console.log('trigger X');
             return this.x + this.age;
@@ -18,18 +17,8 @@ new Vue({
             console.log('trigger Y');
             return this.y + this.age;
         },
-    },
-    computed: {
-        // addX: function(){
-        //     console.log('trigger X');
-        //     return this.x + this.age;
-        // },
-        // addY: function(){
-        //     console.log('trigger Y');
-        //     return this.y + this.age;
-        // },
-        // addXY: function(){
-        //     return this.x + this.y;
-        // }
+        addXY: function(){
+            return this.x + this.y;
+        }
     }
 });
