4d65adb3639d66361599c0382e70e3f8a3a52837
Jerry <jerry.wang@xuenn.com>
2021-01-29 11:41:43 +0800

    v-for loop array, object

--- app.js
+++ app.js
@@ -1,33 +1,18 @@
 new Vue({
     el: "#vue-app",
     data: {
-        age: 18,
-        x: 0,
-        y: 0,
-        error: false,
-        success: false,
-        isholiday: false,
+        name: "jerry",
+        hobby: ["game", "gym", "netflix"],
+        friends: [
+            { name:"lynn", status:"tricky" },
+            { name:"rin",  status:"not sure" },
+            { name:"evan", status:"some worry" }
+        ]
     },
     methods: {
        
     },
     computed: {
-        comClass: function(){
-            return {
-                available: this.available,
-                nearby: this.nearby,
-            }
-        },
-        addX: function(){
-            console.log('trigger X');
-            return this.x + this.age;
-        },
-        addY: function(){
-            console.log('trigger Y');
-            return this.y + this.age;
-        },
-        addXY: function(){
-            return this.x + this.y;
-        }
+
     }
 });
