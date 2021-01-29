a83136f9d78970f6ccea59df9e54a0003240ea12
Jerry <jerry.wang@xuenn.com>
2021-01-29 17:19:08 +0800

    $refs console.log(this.$refs);

--- app.js
+++ app.js
@@ -1,55 +1,15 @@
-var dataObject = {
-    count: 0,
-    title: "from component"
-};
-
-
-Vue.component('first-component', {
-    template: '<div><p>wasa: {{ title }} </p> <button v-on:click="count++">You clicked me {{ count }} times.</button></div>',
-    data: function () {
-      return dataObject;
-    },
-    methods: {
-        // addCount: function()
-        // {
-        //     this.count++;
-        // }
-    },
-  })
-
-
-var one = new Vue({
-    el: "#vue-app-one",
+new Vue({
+    el: "#vue-app",
     data: {
-        title: "one VueApp",
+        title: "vue $ref",
     },
     methods: {
+        sayGetName: function(){
+            console.log(this.$refs);
+            console.log(this.$refs.jerryRef.value);
+        }
+    },
+    computed: {        
         
-    },
-    computed: {
-        comp: function(){
-            return "one comp VueApp comp" + this.name;
-        }
     }
 });
-
-var two = new Vue({
-    el: "#vue-app-two",
-    data: {
-        title: "two VueApp",
-    },
-    methods: {
-        channgeTitle: function(){
-            one.title = "you got changed";
-            one.name = "JJ";
-            this.name = "GG la";
-        }
-    },
-    computed: {
-        comp: function(){
-            return "two comp VueApp comp";
-        }
-    }
-});
-
-two.title = "u will be my shape";
